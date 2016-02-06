16af4a3c3637a2099a39ed1633c653aa801d3e97
Tom Cocozzello <thomas.cocozzello@gmail.com>
2016-02-06 17:13:23 -0600

    Start app that allows user to run a command on the specified server

---
+++ SDS/app/src/androidTest/java/com/example/tom/sds/MainActivityTest.java
@@ -0,0 +1,25 @@
+package com.example.tom.sds;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by Tom on 2/6/2016.
+ */
+public class MainActivityTest extends TestCase {
+
+    public void testOnCreate() throws Exception {
+
+    }
+
+    public void testExecuteRemoteCommand() throws Exception {
+
+    }
+
+    public void testOnCreateOptionsMenu() throws Exception {
+
+    }
+
+    public void testOnOptionsItemSelected() throws Exception {
+
+    }
+}
---
+++ SDS/app/src/main/java/com/example/tom/sds/MainActivity.java
@@ -0,0 +1,107 @@
+package com.example.tom.sds;
+
+import android.app.ListActivity;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.io.ByteArrayOutputStream;
+import java.util.List;
+import java.util.Properties;
+
+import com.jcraft.jsch.ChannelExec;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.Session;
+import android.os.Bundle;
+import android.app.Activity;
+
+public class MainActivity extends ListActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Shut Down all", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+
+                new AsyncTask<Integer, Void, Void>() {
+                    @Override
+                    protected Void doInBackground(Integer... params) {
+                        String response = executeRemoteCommand("tom", "ebsezim2", "192.168.1.15", 22);
+                        Log.d("RESPONSE", response);
+                        return null;
+                    }
+                }.execute(1);
+            }
+        });
+    }
+    public static String executeRemoteCommand(String username,String password,String hostname,int port) {
+        try {
+            JSch jsch = new JSch();
+            Session session = jsch.getSession(username, hostname, port);
+            session.setPassword(password);
+
+            // Avoid asking for key confirmation
+            Properties prop = new Properties();
+            prop.put("StrictHostKeyChecking", "no");
+            session.setConfig(prop);
+
+            session.connect();
+
+            // SSH Channel
+            ChannelExec channelssh = (ChannelExec)
+                    session.openChannel("exec");
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            channelssh.setOutputStream(baos);
+
+            // HAVE USER ENTER THE COMMAND THEY WNAT TO RUN
+            channelssh.setCommand("echo " + password + " | sudo -S poweroff");
+            channelssh.connect();
+            channelssh.disconnect();
+            Log.d("SUCCESS", baos.toString());
+
+            return "SUCCESS" + baos.toString();
+
+        }
+        catch (com.jcraft.jsch.JSchException a)
+        {
+            Log.d("FAILED", "FAILED TO SHUTDOWN");
+            return "FAILED";
+        }
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
