2fbdd4753a5776e65f213834e028e6276ca6fb88
Tom Cocozzello <thomas.cocozzello@gmail.com>
2016-02-15 21:59:59 -0600

    add password alert dialog

--- SDS/app/src/main/java/com/example/tom/sds/MainActivity.java
+++ SDS/app/src/main/java/com/example/tom/sds/MainActivity.java
@@ -1,12 +1,18 @@
 package com.example.tom.sds;
 
+import android.app.AlertDialog;
 import android.app.ListActivity;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.res.Resources;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.View;
 import android.view.Menu;
@@ -16,13 +22,26 @@ import java.io.ByteArrayOutputStream;
 import java.util.List;
 import java.util.Properties;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.jcraft.jsch.ChannelExec;
 import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.JSchException;
 import com.jcraft.jsch.Session;
 import android.os.Bundle;
 import android.app.Activity;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TableRow;
 
 public class MainActivity extends ListActivity {
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,22 +52,60 @@ public class MainActivity extends ListActivity {
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Shut Down all", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(final View view) {
 
-                new AsyncTask<Integer, Void, Void>() {
-                    @Override
-                    protected Void doInBackground(Integer... params) {
-                        String response = executeRemoteCommand("tom", "ebsezim2", "192.168.1.15", 22);
-                        Log.d("RESPONSE", response);
-                        return null;
+                runOnUiThread(new Runnable() {
+                    public void run() {
+                        if (!isFinishing()) {
+
+                            AlertDialog.Builder alert = new AlertDialog.Builder(view.getContext());
+
+                            alert.setTitle("Server Password");
+
+                            LinearLayout layout = new LinearLayout(view.getContext());
+                            layout.setOrientation(LinearLayout.VERTICAL);
+                            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                                    LinearLayout.LayoutParams.FILL_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+                            params.setMargins(35, 0, 35, 0);
+
+                            final EditText textBox = new EditText(MainActivity.this);
+                            layout.addView(textBox, params);
+
+                            alert.setView(layout);
+
+                            alert.setPositiveButton("Run", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int whichButton) {
+                                    String text = textBox.getText().toString();
+
+                                    new AsyncTask<String, Void, Void>() {
+                                        @Override
+                                        protected Void doInBackground(String... params) {
+                                            String tex = params[0];
+                                            String response = executeRemoteCommand("admin", tex, "192.168.1.19", 22);
+                                            Log.d("RESPONSE", response);
+                                            return null;
+                                        }
+                                    }.execute(text);
+                                }
+                            });
+
+//                            Snackbar.make(view, "ip Powering Off", Snackbar.LENGTH_LONG)
+//                                    .setAction("Action", null).show();
+                            alert.show();
+
+                        }
                     }
-                }.execute(1);
+                });
+
+
             }
         });
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
-    public static String executeRemoteCommand(String username,String password,String hostname,int port) {
+
+    public static String executeRemoteCommand(String username, String password, String hostname, int port) {
         try {
             JSch jsch = new JSch();
             Session session = jsch.getSession(username, hostname, port);
@@ -68,20 +125,20 @@ public class MainActivity extends ListActivity {
             channelssh.setOutputStream(baos);
 
             // HAVE USER ENTER THE COMMAND THEY WNAT TO RUN
-            channelssh.setCommand("echo " + password + " | sudo -S poweroff");
+            //channelssh.setCommand("echo " + password + " | sudo -S poweroff");
+            channelssh.setCommand("poweroff");
             channelssh.connect();
             channelssh.disconnect();
             Log.d("SUCCESS", baos.toString());
 
             return "SUCCESS" + baos.toString();
 
-        }
-        catch (com.jcraft.jsch.JSchException a)
-        {
+        } catch (JSchException a) {
             Log.d("FAILED", "FAILED TO SHUTDOWN");
             return "FAILED";
         }
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -102,5 +159,45 @@ public class MainActivity extends ListActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.example.tom.sds/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.example.tom.sds/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
     }
 }
